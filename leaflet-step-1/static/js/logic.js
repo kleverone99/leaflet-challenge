f6a7238f1ef4a25f19ca3159da0088e39d079a0e
kleverone99 <75403103+kleverone99@users.noreply.github.com>
2021-05-16 23:42:34 -0400

    Add files via upload

---
+++ leaflet-step-1/static/js/logic.js
@@ -0,0 +1,71 @@
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+});
+
+var quakeMap = L.map("map-id", {
+    center: [40.73, -104.0059],
+    zoom:3
+});
+
+lightMap.addTo(quakeMap);
+
+// Update the dot colors based on depth
+function colordot (depth) {
+    if (depth <= 15) {
+        return ("green");    
+    } else if (depth <= 30) {
+        return ("yellow");
+    } else if (depth <= 100) {
+        return ("orange");
+    } else {
+        return ("red");
+    }
+
+}
+
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Store the API endpoint inside queryURL
+
+d3.json(queryURL).then(function(data) {
+
+    var earthquakes = data.features;
+    console.log(earthquakes.length);
+
+// Create each marker coordinate and push to empty array
+
+    earthquakes.forEach(element => {
+        lat = element['geometry']['coordinates'][1];
+        lng = element['geometry']['coordinates'][0];
+        mag = element['properties']['mag'];
+        depth = element['geometry']['coordinates'][2];
+    
+        L.circleMarker([lat, lng], {
+            radius: mag * 2,
+            fillOpacity: 0.8,
+            stroke: true,
+            weight: 1,
+            color: colordot(depth),
+            fillColor: colordot(depth)
+        }).bindPopup("Depth: " + element['geometry']['coordinates'][2] + " meters" + 
+        "<br> Magnitude: " + element['properties']['mag'] + " ML")
+        .addTo(quakeMap);
+
+    });
+    
+});
+
+var legend = L.control({position: "bottomright"});
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var legendInfo = 
+        "<span style='color:green'>&#9673</span> 0 - 15 m depth <br> <span style='color:yellow'>&#9673</span> 15.1 - 30 m depth <br> <span style='color:orange'>&#9673</span> 30.1 - 100 m depth <br> <span style='color:red'>&#9673</span> Greater than 100 m depth";
+
+    div.innerHTML = legendInfo;
+
+    return div;
+};
+legend.addTo(quakeMap);
